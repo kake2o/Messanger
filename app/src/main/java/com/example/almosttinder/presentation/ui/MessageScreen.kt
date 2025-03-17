42086f48d7ecd8878bec793deb6dde22b93aec1d
kaketo <drozhenko.i89@gmail.com>
2025-03-17 19:56:34 +0200

    added for future usage

---
+++ app/src/main/java/com/example/almosttinder/presentation/ui/MessageScreen.kt
@@ -0,0 +1,27 @@
+package com.example.almosttinder.presentation.ui
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun MessageScreen() {
+    LazyColumn(modifier = Modifier.fillMaxSize()
+        .padding(20.dp)
+        .clip(RoundedCornerShape(20.dp)
+        )
+    ) {
+        MessageCard()
+    }
+}
+
+@Composable
+fun MessageCard() {
+    Card() {  }
+}
