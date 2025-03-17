926fae0cfc636465b13ca854cf18185d8e89b7ef
kaketo <drozhenko.i89@gmail.com>
2025-03-17 19:56:23 +0200

    renamed

--- app/src/main/java/com/example/almosttinder/presentation/ui/LoginScreen.kt
+++ app/src/main/java/com/example/almosttinder/presentation/ui/LoginScreen.kt
@@ -4,23 +4,29 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.almosttinder.ui.theme.AlmostTinderTheme
+import androidx.compose.ui.unit.sp
+import com.example.almosttinder.R
+
 
 @Composable
-fun StartScreen() {
+fun LoginScreen() {
     val fontRobotoRegular = FontFamily(Font(resId = R.font.roboto_condensed_regular))
     val fontRobotoSemiBold = FontFamily(Font(resId = R.font.roboto_condensed_semibold))
 
@@ -56,8 +62,8 @@ fun StartScreen() {
                     .align(Alignment.CenterHorizontally)
             )
             TextField(
-                value = textUsername,
-                onValueChange = { textUsername = it },
+                value = textPassword,
+                onValueChange = { textPassword = it },
                 placeholder = {
                     Text(
                         text = stringResource(id = R.string.loginPassword),
@@ -89,13 +95,5 @@ fun StartScreen() {
                 )
             }
         }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun PreviewScreen() {
-    AlmostTinderTheme(dynamicColor = false) {
-        StartScreen()
     }
 }
