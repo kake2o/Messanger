df92dc3cd0d09aafb238c064256ba5dc240ab7db
kaketo <drozhenko.i89@gmail.com>
2025-03-20 11:06:02 +0200

    added floating button

--- app/src/main/java/com/example/almosttinder/ui/theme/Color.kt
+++ app/src/main/java/com/example/almosttinder/ui/theme/Color.kt
@@ -3,11 +3,13 @@ package com.example.almosttinder.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val PrimaryLight = Color(0xFFF1B40C)
-val SecondaryLight = Color(0xFFD9DEB2)
+val SecondaryLight = Color(0xFFF1D6B6)
 val TertiaryLight = Color(0xFFFFFFE5)
-val BackgroundWhite = Color(0xFFFFFFFF)
+val BackgroundLight = Color(0xFFFFFFFF)
+val onPrimaryContainerLight = Color(0xFFC7C7C7)
 
 val PrimaryDark = Color(0xFF6650a4)
 val SecondaryDark = Color(0xFF625b71)
 val TertiaryDark = Color(0xFF7D5260)
 val BackgroundBlack = Color(0xFF000000)
+val onPrimaryContainerDark = Color(0xFF686868)
